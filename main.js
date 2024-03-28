68874da0143e4dfe4032191a8ca2c5b73b702e3d
Abubaker Siddiqui <162192538+AbubakerSiddiqui001@users.noreply.github.com>
2024-03-28 14:56:47 +0500

    code-day-05

---
+++ main.js
@@ -0,0 +1,18 @@
+// Find total guest
+var guest = [
+    "Albert Einsten",
+    "Billgates",
+    "Sam Altman",
+    "Elon Musk",
+    "Zukerburg",
+];
+console.log("I'am inviting ".concat(guest.length, " people to dinner"));
+// Collect all cities in One Array
+var countries = ["Japan", "Canada", "Iceland", "China", "Noreway"];
+console.log("Countreis I'd like to visit", countries);
+var book = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkein",
+    yearPublished: 1937
+};
+console.log("Book info: ".concat(book.title, ", by ").concat(book.author, ", published in ").concat(book.yearPublished));
