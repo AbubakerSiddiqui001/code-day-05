74dafe3038a669fac03b1fdb0ad2b0eb27ae7b14
Abubaker Siddiqui <162192538+AbubakerSiddiqui001@users.noreply.github.com>
2024-03-28 14:56:00 +0500

    code-day-05

---
+++ main.ts
@@ -0,0 +1,21 @@
+// Find total guest
+let guest: string[] = [
+  "Albert Einsten",
+  "Billgates",
+  "Sam Altman",
+  "Elon Musk",
+  "Zukerburg",
+];
+console.log(`I'am inviting ${guest.length} people to dinner`);
+
+// Collect all cities in One Array
+let countries: string[] = ["Japan", "Canada", "Iceland", "China", "Noreway"];
+console.log("Countreis I'd like to visit" , countries);
+
+
+let book: {title: string, author: string, yearPublished: number} = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkein",
+    yearPublished: 1937
+};
+console.log(`Book info: ${book.title}, by ${book.author}, published in ${book.yearPublished}`);
